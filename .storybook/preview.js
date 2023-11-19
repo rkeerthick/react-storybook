4cf750d279f0c25a359950715e2ea3968420bbaa
rkeerthick <rkeerthick1@gmail.com>
2023-11-19 17:55:47 +0530

    preview.js updated

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,32 +1,28 @@
 /** @type { import('@storybook/react').Preview } */
 import React from "react";
-import Center from "../src/components/Center/Center";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    options: {
-      storySort: (a, b) =>
-        a.id === b.id
-          ? 0
-          : a.id.localeCompare(b.id, undefined, { numeric: true }),
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
     },
   },
-  decorators: [
-    (story) => (
-      <ChakraProvider>
-        {/* <Center>{story()}</Center> */}
-        <Box>{story()}</Box>
-      </ChakraProvider>
-    ),
-  ],
+  options: {
+    storySort: (a, b) =>
+      a.id === b.id
+        ? 0
+        : a.id.localeCompare(b.id, undefined, { numeric: true }),
+  },
 };
 
-export default preview;
+export const decorators = [
+  (story) => (
+    <ChakraProvider>
+      {/* <Center>{story()}</Center> */}
+      <Box>{story()}</Box>
+    </ChakraProvider>
+  ),
+];
