69748470d3107204ec0e3a05393f5a3ffe65f36e
rkeerthick <rkeerthick1@gmail.com>
2023-11-19 17:51:52 +0530

    chakra ui is added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 /** @type { import('@storybook/react').Preview } */
-import React from 'react';
-import Center from '../src/components/Center/Center';
+import React from "react";
+import Center from "../src/components/Center/Center";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -12,10 +14,19 @@ const preview = {
     },
     options: {
       storySort: (a, b) =>
-      a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
+        a.id === b.id
+          ? 0
+          : a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
   },
-  decorators: [story => <Center>{story()}</Center>]
+  decorators: [
+    (story) => (
+      <ChakraProvider>
+        {/* <Center>{story()}</Center> */}
+        <Box>{story()}</Box>
+      </ChakraProvider>
+    ),
+  ],
 };
 
 export default preview;
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,14 +1,13 @@
-
 import Input from "./Input";
 
 export default {
-    title: 'Form/Input',
-    component: Input,
-    // decorators: [story => <Center>{story()}</Center>]
-}
+  title: "Form/Input",
+  component: Input,
+  // decorators: [story => <Center>{story()}</Center>]
+};
 
-export const Small = () => <Input variant="small">Small</Input>
-export const Medium = () => <Input variant="medium">Medium</Input>
-export const Large = () => <Input variant="large">Large</Input>
+export const Small = () => <Input variant="small">Small</Input>;
+export const Medium = () => <Input variant="medium">Medium</Input>;
+export const Large = () => <Input variant="large">Large</Input>;
 
-Small.storyName = "Small Input"
+Small.storyName = "Small Input";
